8e9869c4030ad1b6a60fe404ddb18004135ff965
anton <anton.danilov@ylab.io>
2020-09-09 23:53:47 +0300

    added splash-screen

--- android/app/src/main/java/com/reactnativeskeleton/MainActivity.java
+++ android/app/src/main/java/com/reactnativeskeleton/MainActivity.java
@@ -1,6 +1,9 @@
 package com.reactnativeskeleton;
 
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
+import android.os.Bundle;
+
 
 public class MainActivity extends ReactActivity {
 
@@ -8,6 +11,11 @@ public class MainActivity extends ReactActivity {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+     SplashScreen.show(this);
+     super.onCreate(savedInstanceState);
+   }
   @Override
   protected String getMainComponentName() {
     return "reactNativeSkeleton";
--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {
     StatusBar,
 } from 'react-native';
+import SplashScreen from 'react-native-splash-screen'
 import Root from "./navigators";
 import { Provider } from 'react-redux'
 import store from './store/store'
 
 
 const App: () => React$Node = () => {
+    useEffect(()=> {
+        SplashScreen.hide()
+    },[])
     return (
         <Provider store={store}>
-            <StatusBar barStyle="light-content"/>
+            <StatusBar barStyle="dark-content"/>
             <Root/>
         </Provider>
     );
